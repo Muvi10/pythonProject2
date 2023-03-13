6871d09dc1b0783fa924e371008ad0c064be2b6e
Abdu_zjr2 <g@gmail.com>
2023-03-13 16:44:40 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+class Cats:
+
+    def __init__(self,name):
+        self.name = name
+        self.age = 1
+        def display_info(self):
+            print(f"Name : {self.name} Color: {self.color} Age: {self.age} Character: {self.age}")
+
+marusia = Cats("marusia")
+marusia.color = "black"
+marusia.age = "4 years"
+marusia.character = "lazy,fat,meow"
+marusia.display_info()
+
+angela = Cats("angela")
+angela.color = "red"
+angela.age = "3 months"
+angela.character = "smart,small,good,sleeps"
+angela.display_info()
